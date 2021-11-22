fb33c7dc115496f980db3dc07d2df581768e9a53
Nelson Delpozo <nelson.delpozo99@gmail.com>
2021-11-22 12:29:04 -0600

    primitives exercises

---
+++ strings.js
@@ -0,0 +1,9 @@
+/*let myName = "Nelson";
+console.log("That's awesome! " + myName + " is my name too!");*/
+
+let myName = "Nelson";
+let myHobby = ["guitar", "coding"];
+let myHometown = "I was born in New York City...get a rope.";
+console.log(`Hello! My name is ${myName}, \n\t and my hobbies are ${myHobby[0]} and ${myHobby[1]}.\n\t\t ${myHometown}`);
+
+
