73c40a4f5b13fab4354c19d9c107c5ec54a34f5b
Nelson Delpozo <nelson.delpozo99@gmail.com>
2021-11-30 21:18:43 -0600

    loops exercises

---
+++ js/for_loops.js
@@ -0,0 +1,82 @@
+//for loops exercises//
+
+console.log("connected");
+///multiplication table, returns 1-10 times the passed argument//
+function multiplicationTable(num){
+    for(let i = 1; i < 11; i++){
+        console.log(num + " x " + i + " = " + (num*i) )
+    }
+}
+
+multiplicationTable(7);
+
+////////////////////////////////////
+//tenRandomNumbers generates ten random numbers using a for loop and logs the numbers and whether they are even or odd//
+function tenRandomNumbers(){
+
+    for (let i = 0; i < 10; i++) {
+        let num = (Math.floor(Math.random() * (200 - 20 + 1)) + 20);
+        if(num % 2 === 0){
+            console.log(num + " is even")
+        }else console.log(num + " is odd")
+
+    }
+
+}
+tenRandomNumbers();
+
+
+///////////////////////////////////
+//recreating this output:
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+// 7777777
+// 88888888
+// 999999999
+
+function makePyramidOfNumbers(){
+    let str = "";
+    for(let i = 1; i < 10; i++){
+        str += i.toString();
+        console.log(str)
+        // str += i;
+        // let newStr = str.toString();
+        // console.log(newStr);
+    }
+
+}
+makePyramidOfNumbers();
+
+///////////////////////////////////
+//logs 100-5 in increments of 5//
+
+function forLoop100to5(){
+
+    for(let i = 100; i > 0; i--){
+        if(i % 5 === 0){
+            console.log(i)
+        }
+    }
+}
+forLoop100to5();
+
+//////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
