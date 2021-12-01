3736062c230ffd88809c0d53ebbe9fb4bd41ed50
Nelson Delpozo <nelson.delpozo99@gmail.com>
2021-12-01 09:01:35 -0600

    morning commit

--- js/for_loops.js
+++ js/for_loops.js
@@ -85,6 +85,10 @@ function oddNumbers(){
 
 oddNumbers();
 
+//////////////////////////////
+
+
+
 
 
 
---
+++ js/while.js
@@ -0,0 +1,23 @@
+console.log("connected");
+
+//while loops exercises//
+
+let i = 2;
+while(i <= 65536) {
+
+    console.log(i);
+
+    i *= 2;
+}
+
+/////////////////////////////
+//generates a random number between 50 - 100//
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+
+//generates random number between 1 - 5//
+
+Math.floor(Math.random() * 5) + 1;
+
+
