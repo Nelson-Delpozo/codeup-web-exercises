ca1624a6304c5668badfcfedfa61023c0e8bac2f
Nelson Delpozo <nelson.delpozo99@gmail.com>
2021-11-30 22:48:59 -0600

    completed oddNumbers

--- js/for_loops.js
+++ js/for_loops.js
@@ -65,8 +65,25 @@ forLoop100to5();
 
 //////////////////////////////////
 
+function oddNumbers(){
+   // let num =  prompt("Enter an odd number")
+    let num;
+    do{
+      num = prompt("Enter an odd number")
+    }while (num % 2 === 0);
+    console.log("Number to skip: " + num);
+    for(let i = 1; i < 51; i++){
+        if(i % 2 === 0){
+            continue
+        }
+        if(i - num === 0){
+            console.log("Yikes! Skipping number: " + num);
+        }else
+        console.log("Here is an odd number: " + i)
+    }
+}
 
-
+oddNumbers();
 
 
 
