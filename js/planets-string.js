055747d224ae6a81a632db9ab7ba22c74f8daac4
Nelson Delpozo <nelson.delpozo99@gmail.com>
2021-12-03 09:02:39 -0600

    finished planets exercises

--- js/planets-string.js
+++ js/planets-string.js
@@ -12,18 +12,38 @@ console.log("connected");
      * planetsArray.
      * console.log planetsArray to check your work
      */
-
-    // console.log(planetsArray);
+    planetsArray = planetsString.split("|");
+    console.log(planetsArray);
 
     /**
      * TODO:
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
-     *
      * BONUS:
      * Create another string that would display your planets in an undordered
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
 
+    let newString = planetsArray.join("\n");
+    console.log(newString);
+
+    ////////////////////////////////////////////
+
+    let listString = "<ul>";
+    planetsArray.forEach(function(planet){
+
+        listString += "<li>" + planet + "</li>";
+        console.log(listString);
+
+    });
+        listString += "</ul>";
+    console.log(listString);
+
+
 })();
+
+
+
+
+
