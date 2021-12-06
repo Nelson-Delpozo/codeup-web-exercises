8842c255c465d7327bae2b6cbfcab4d47b44b9db
Nelson Delpozo <nelson.delpozo99@gmail.com>
2021-12-06 08:53:09 -0600

    fixed alignment

--- js/objects.js
+++ js/objects.js
@@ -62,8 +62,6 @@ console.log("connected");
 
 
 
-
-
     /** TODO:
      * Create an array of objects that represent books and store it in a
      * variable named `books`. Each object should have a title and an author
