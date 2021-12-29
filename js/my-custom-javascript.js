d5073088fe9f8a7a6c6136e0d1ca85420196d2af
Nelson Delpozo <nelson.delpozo99@gmail.com>
2021-12-28 22:43:13 -0600

    selectors exercises

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -4,5 +4,27 @@ $(document).ready(function() {
 
     // INSERT JAVASCRIPT CODE SAMPLE HERE
     alert("The DOM has finished loading!");
+//////////////////////////////////////////
+
+    let contents = $('#codeup').html();
+    alert(contents)
+
+////////////////////////////////////////////
+
+    $('.important').css('background-color', 'yellow');
+
+//////////////////////////////////////////
+
+    $('p').css('font-size', '14px');
+
+    ////////////////////////////////////////
+
+    $('.important, p').css('background-color', 'orange');
+
+    ///////////////////////////////////////////////
+
+    $('*').css('border', '1px solid #F00');
+
+    ////////////////////////////////////////////////////
 
 });
