2ffecbe7f309c7c15d8beda1bd73a8f03c544fbf
Nelson Delpozo <nelson.delpozo99@gmail.com>
2021-12-28 22:51:53 -0600

    completed id selectors

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -3,28 +3,34 @@
 $(document).ready(function() {
 
     // INSERT JAVASCRIPT CODE SAMPLE HERE
-    alert("The DOM has finished loading!");
-//////////////////////////////////////////
-
-    let contents = $('#codeup').html();
-    alert(contents)
-
-////////////////////////////////////////////
-
-    $('.important').css('background-color', 'yellow');
-
-//////////////////////////////////////////
-
-    $('p').css('font-size', '14px');
-
-    ////////////////////////////////////////
-
-    $('.important, p').css('background-color', 'orange');
-
-    ///////////////////////////////////////////////
-
-    $('*').css('border', '1px solid #F00');
+//     alert("The DOM has finished loading!");
+// //////////////////////////////////////////
+//
+//     let contents = $('#codeup').html();
+//     alert(contents)
+//
+// ////////////////////////////////////////////
+//
+//     $('.important').css('background-color', 'yellow');
+//
+// //////////////////////////////////////////
+//
+//     $('p').css('font-size', '14px');
+//
+//     ////////////////////////////////////////
+//
+//     $('.important, p').css('background-color', 'orange');
+//
+//     ///////////////////////////////////////////////
+//
+//     $('*').css('border', '1px solid #F00');
 
     ////////////////////////////////////////////////////
+
+    let message = $('#first-list-item').html();
+    alert(message);
+
+    let secondMessage = $('#main-list').html();
+    alert(secondMessage);
 
 });
