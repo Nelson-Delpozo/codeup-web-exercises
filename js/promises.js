1a6cb0a189551dbb21cda29c34f281781c637d8d
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-04 15:03:14 -0600

    my own promise

--- js/promises.js
+++ js/promises.js
@@ -1,1 +1,24 @@
 "use strict";
+
+function getGithubCommits(username) {
+    return fetch(`https://api.github.com/users/${username}/events`)
+        .then(response => response.json())
+}
+getGithubCommits("Nelson-Delpozo").then(events => {
+    events.forEach(event => {
+        // do something with each username
+        console.log(event);
+    });
+}).catch(error => console.error(error));
+
+
+function wait(delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+        }, delay);
+    });
+}
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
