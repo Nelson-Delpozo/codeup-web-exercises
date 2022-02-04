2d034c4af609fea61facd4d83848ef8cb54ed641
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-04 15:13:12 -0600

    finished promises

--- js/promises.js
+++ js/promises.js
@@ -13,12 +13,12 @@ getGithubCommits("Nelson-Delpozo").then(events => {
 
 
 function wait(delay) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         setTimeout(() => {
-
+            resolve();
         }, delay);
     });
 }
-wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+wait(1000).then(() => console.log(`You'll see this after 1 second`));
+wait(3000).then(() => console.log(`You'll see this after 3 seconds`));
 
