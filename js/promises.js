97d6102deac915f8abd7f0479816f03e6d3d6f32
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-04 16:35:08 -0600

    fetch

--- js/promises.js
+++ js/promises.js
@@ -22,3 +22,21 @@ function wait(delay) {
 wait(1000).then(() => console.log(`You'll see this after 1 second`));
 wait(3000).then(() => console.log(`You'll see this after 3 seconds`));
 
+const reviewObj = {
+    restaurant_id: 1,
+    name: 'Codey\'s2',
+    rating: 5,
+    comments: "This is a really good place for coding and eating things"
+};
+const url = 'https://codeup-restful-example.glitch.me/reviews';
+const options = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(reviewObj),
+};
+fetch(url, options)
+    .then( response => console.log(response) ) /* review was created successfully */
+    .catch( error => console.error(error) ); /* handle errors */
+
