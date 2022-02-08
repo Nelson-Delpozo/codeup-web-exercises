f91464d1040e37a980dd51743e8a29c585499816
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-08 09:15:49 -0600

    morning warmup

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -197,3 +197,14 @@ function makeSuperPet(array){
 }
 
 console.log(makeSuperPet(pets));
+
+// LAURA WARMUP 2/8
+
+// Write a function that removes any duplicates from an array.
+//     Input: [2,3,5,8,3,1,2,6,3,2,7,8,2,4,7]
+// expected output: [2,3,5,8,1,6,7,4]
+
+function removeDuplicates(array){
+    return Array.from(new Set(array))
+}
+console.log(removeDuplicates([2,3,5,8,3,1,2,6,3,2,7,8,2,4,7]));
